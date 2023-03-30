b5a997b453f965419d239c760040ec0aa91bd0b3
Chris Figueroa <Chris.FigueroaPerez@gmail.com>
2023-03-30 15:51:38 -0400

    📦 NEW: Complete AddEntryForm Component

--- src/App.js
+++ src/App.js
@@ -1,13 +1,20 @@
 import React, { useState } from 'react';
 import DisplayEntries from './Components/DisplayEntries/DisplayEntries';
+import AddEntryForm from './Components/AddEntry/AddEntryForm';
 
 function App() {
 
   const [enteries, setEntries] = useState([{weight: 270, date: "11-30-2022"},{weight: 265, date: "12-20-2022"}])
 
+  function logNewEntry(entry){
+    let tempEntries = [...enteries, entry]
+    setEntries(tempEntries)
+  }
+
   return (
     <div>
       <DisplayEntries allEnteries={enteries} />
+      <AddEntryForm logNewEntry={logNewEntry}/>
     </div>
   );
 }
