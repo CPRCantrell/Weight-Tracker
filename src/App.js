b1c7f54e16b780cf774f059e22a71b748d29bed0
Chris Figueroa <Chris.FigueroaPerez@gmail.com>
2023-03-30 17:03:56 -0400

    👌 IMPROVE: Add stylization to the app

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import DisplayEntries from './Components/DisplayEntries/DisplayEntries';
 import AddEntryForm from './Components/AddEntry/AddEntryForm';
 import EntriesChartTracker from './Components/EntriesChartTracker/EntriesChartTracker';
+import './App.css';
 
 function App() {
 
@@ -13,10 +14,23 @@ function App() {
   }
 
   return (
-    <div>
-      <DisplayEntries allEnteries={enteries} />
-      <AddEntryForm logNewEntry={logNewEntry}/>
-      <EntriesChartTracker allEnteries={enteries} />
+    <div className='container-fluid'>
+      <div className='row'>
+        <h1 style={{margin: '1em'}}>weight<small className='text-muted'>Tracker</small></h1>
+        <div className='col-md-6'>
+          <div className='border-box'>
+            <AddEntryForm logNewEntry={logNewEntry}/>
+          </div>
+          <div className='border-box'>
+            <DisplayEntries allEnteries={enteries} />
+          </div>
+        </div>
+        <div className='col-md-6'>
+          <div className='border-box'>
+            <EntriesChartTracker allEnteries={enteries} />
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
