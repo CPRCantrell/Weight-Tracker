72a32922a4ddbc39c5619302e47679af8d6b05b4
Chris Figueroa <Chris.FigueroaPerez@gmail.com>
2023-03-30 16:27:11 -0400

    📦 NEW: Completed EntriesChartTracker Component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React, { useState } from 'react';
 import DisplayEntries from './Components/DisplayEntries/DisplayEntries';
 import AddEntryForm from './Components/AddEntry/AddEntryForm';
+import EntriesChartTracker from './Components/EntriesChartTracker/EntriesChartTracker';
 
 function App() {
 
-  const [enteries, setEntries] = useState([{weight: 270, date: "11-30-2022"},{weight: 265, date: "12-20-2022"}])
+  const [enteries, setEntries] = useState([{weight: 270, date: "11-30-2022"}])
 
   function logNewEntry(entry){
     let tempEntries = [...enteries, entry]
@@ -15,6 +16,7 @@ function App() {
     <div>
       <DisplayEntries allEnteries={enteries} />
       <AddEntryForm logNewEntry={logNewEntry}/>
+      <EntriesChartTracker allEnteries={enteries} />
     </div>
   );
 }
